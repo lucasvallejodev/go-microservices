12e378a33dbb0b5434de24926b4eeec5f190d87a
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2024-04-13 00:02:58 +0200

    feat: create auth server

---
+++ auth-server/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"auth-server/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
