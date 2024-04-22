0c60718ce424e5e0066fad93a2f9358a0bd7616a
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2024-04-22 22:03:57 +0200

    feat: add log entry case to broker

--- broker-server/cmd/api/handlers.go
+++ broker-server/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth, omoitempty"`
+	Log    LogPayload  `json:"log, omoitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +44,8 @@ func (app *Config) HandleSubmition(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authentica(w, requestPayload.Auth)
+	case "log":
+		app.logEntry(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknown action"), http.StatusBadRequest)
 	}
@@ -87,6 +95,51 @@ func (app *Config) authentica(w http.ResponseWriter, a AuthPayload) {
 	payload.Error = false
 	payload.Message = jsonFromService.Message
 	payload.Data = jsonFromService.Data
+
+	app.WriteJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) logEntry(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	request, err := http.NewRequest("POST", "http://logger-server/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		app.errorJSON(w, errors.New("error calling logger server"), response.StatusCode)
+		return
+	}
+
+	var jsonFromService jsonResponse
+
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		app.errorJSON(w, errors.New(jsonFromService.Message), response.StatusCode)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = jsonFromService.Message
 
 	app.WriteJSON(w, http.StatusOK, payload)
 }
