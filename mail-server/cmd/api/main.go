a5b31c01b0d17802d39a1a69183a656fa6ea3072
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2024-05-02 23:11:59 +0200

    feat: add send email logic

--- mail-server/cmd/api/main.go
+++ mail-server/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createEmail(),
+	}
 
 	log.Println("Starting server on port %s", webPort)
 
@@ -25,4 +30,20 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createEmail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	mail := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+	}
+
+	return mail
 }
